2cebb22ba26e2e067c709ad6536bd7a63dddfbd9
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2020-09-10 15:17:55 -0300

    Update Ex_45.py

--- Ex_45.py
+++ Ex_45.py
@@ -1,1 +1,17 @@
-#Crie um programa que fala o computador jogar JOkenpô com você
+#Crie um programa que fala o computador jogar Jokenpô com você
+
+from random import randint
+from datetime import time
+
+vJogador = int(input('Suas Opções: '
+                    '\n[ 0 ] PEDRA'
+                    '\n[ 1 ] PAPEL'
+                    '\n[ 2 ] TESOURA'))
+
+
+print('JO')
+print('KEN')
+print('PO')
+
+
+print('Computador jogou')
