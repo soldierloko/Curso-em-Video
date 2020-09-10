b6f25b063dd42aeb2436e28366bd194af0bf8acb
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2020-09-10 19:28:39 -0300

    Ex

--- Ex_45.py
+++ Ex_45.py
@@ -1,17 +1,55 @@
 #Crie um programa que fala o computador jogar Jokenpô com você
 
 from random import randint
-from datetime import time
+from time import sleep
+
+
 
 vJogador = int(input('Suas Opções: '
                     '\n[ 0 ] PEDRA'
                     '\n[ 1 ] PAPEL'
-                    '\n[ 2 ] TESOURA'))
+                    '\n[ 2 ] TESOURA '))
 
+itens = ('Pedra','Papel','Tesoura')
+
+vComputador = randint(0,2)
 
 print('JO')
+sleep(1)
 print('KEN')
-print('PO')
+sleep(1)
+print('PO!!!')
+sleep(1)
 
+print('-='*11)
+print('Computador jogou {}'.format(itens[vComputador]))
+print('Jogador jogou {}'.format(itens[vJogador]))
+print('-='*11)
 
-print('Computador jogou')
+if vComputador ==0:
+    if vJogador == 0:
+        print('Empate')
+    elif vJogador == 1:
+        print('Jogador Vence')
+    elif vJogador == 2:
+        print('Computador Vence')
+    else:
+        print('Jogada Inválida!')
+elif vComputador ==1:
+    if vJogador == 0:
+        print('Computador Vence')
+    elif vJogador == 1:
+        print('Empate')
+    elif vJogador == 2:
+        print('Jogador Vence')
+    else:
+        print('Jogada Inválida!')
+elif vComputador ==2:
+    if vJogador == 0:
+        print('Jogador Vence')
+    elif vJogador == 1:
+        print('Computador Vence')
+    elif vJogador == 2:
+        print('Empate')
+    else:
+        print('Jogada Inválida!')
---
+++ Ex_47.py
@@ -0,0 +1,6 @@
+#Crie um programa que mostre na tela todos os numeros pares que estão no intervalo entre 1 e 50
+
+for i in range(1,50):
+    if (i % 2 == 0):
+        print(i)
+    
