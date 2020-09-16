d7f4e0bda1160197ca151d5bb23098ba50dddd73
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2020-09-15 21:14:29 -0300

    Ex60

--- Ex_59.py
+++ Ex_59.py
@@ -12,3 +12,31 @@
 v1 = int(input('Primeiro Valor: '))
 v2 = int(input('Segundo Valor: '))
 
+op = 0
+while op != 5:
+    print('''[ 1 ] Somar
+             [ 2 ] Multiplicar
+             [ 3 ] Maior
+             [ 4 ] Novos Números
+             [ 5 ] Sair do Programa''')
+    op = int(input('Qual a sua opção: '))
+    if op == 1:
+        print('A soma entre {} e {} é {}'.format(v1,v2,v1+v2))
+    elif op == 2:
+        print('A multiplicação entre {} e {} é {}'.format(v1,v2,v1*v2))
+    elif op == 3:
+        if v1 > v2:
+            maior = v1
+        else:
+            maior = v2
+        print('O Maior número entre {} e {} é {}'.format(v1,v2,maior))
+    elif op == 4:
+        v1 = int(input('Primeiro Valor: '))
+        v2 = int(input('Segundo Valor: '))
+    elif op == 5:
+        print('Finalizando...')
+
+    else:
+        print('Opção Inválida, tente novamente!')
+    print('=-='*10)
+print('Fim do Programa, volte sempre!')
--- Ex_60.py
+++ Ex_60.py
@@ -1,3 +1,10 @@
 # Faça um programa que leia um número qualquer e mostre o seu fatorial.
 
 #Ex: 5! = 5x4x3x2x1=120
+
+from math import factorial
+
+n = int(input('Digite um número: '))
+f = factorial(n)
+
+print('O fatorial de {} é {}'.format(n,f))
