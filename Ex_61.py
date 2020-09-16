42a75b00630b709888ff98c23fd37b360c55243d
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2020-09-16 19:22:23 -0300

    Ex61

--- Ex_61.py
+++ Ex_61.py
@@ -1,1 +1,13 @@
 #Refaça o desafio 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando estrutura While
+
+termo = int(input('Primeiro Termo: '))
+razao = int(input('Qual a Razão: '))
+
+cont = 1
+
+while cont <=10:
+    print('{} - '.format(termo),end='')
+    termo +=razao
+    cont +=1
+
+
