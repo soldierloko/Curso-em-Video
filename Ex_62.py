89c2bc0ea80d97c6a47a2d2f72bbdc207f11983b
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2020-09-20 13:53:54 -0300

    Mundo 2

--- Ex_62.py
+++ Ex_62.py
@@ -1,1 +1,18 @@
 #Melhore o desafio 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele disser que quer mostrar 0 termos
+print('Gerador de PA')
+print('-='*10)
+primeiro = int(input('Primeiro Termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print('{} > '.format(termo), end='')
+        termo += razao
+        cont +=1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados.'.format(total))
--- Ex_64.py
+++ Ex_64.py
@@ -1,1 +1,14 @@
 #Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final mostre quantos números foram digitados e qual foi a soma entre eles (Desconsiderando o Flag)
+
+p = 0
+cont = 0
+total = 0
+n = 0
+
+while n != 999:
+    n = int(input('Digite um número [999 para parar]: '))
+    if n != 999:
+        total = total + n
+        cont += 1
+
+print('Você digitou {} números e a soma entre eles foi {}'.format(cont,total))
