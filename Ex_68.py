daf045881169646988c2694f825fa7fa451b7ff8
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2020-10-01 20:14:59 -0300

    Update68

---
+++ Ex_68.py
@@ -0,0 +1,6 @@
+#Faça um programa que jogue par ou impar com o computador. O jogo só será interrompido quando o jogador PERDER, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+from random import randint
+
+cont = 0
+while True:
+    n = 0
