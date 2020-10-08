8a1d57ef8bbc604cbb4517611198755a37fc280b
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2020-10-08 17:52:20 -0300

    Last73

--- Ex_73.py
+++ Ex_73.py
@@ -3,3 +3,9 @@
 #Os Ultimos 4 colocados da tabela
 #Uma lista com os times em ordem alfabética
 #Em que posição na tabela está o time da Chapecoense
+
+time = 'Atlético-MG','Flamengo','São Paulo','Internacional','Palmeiras','Fluminense','Santos','Sport','Fortaleza EC','Vasco da Gama','Grêmio','Bahia','Corinthians','Atlético Goianiense','Botafogo','Athletico-PR','Ceará','Coritiba','Bragantino','Goiás'
+
+print(f'Os cinco primeiros colocados são {time[:5]}')
+print(f'Os Últimos 4 colocados são {time[-4:]}')
+print(sorted(time))
