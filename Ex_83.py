c21d382fc0af5d1d2730deb2b8a3d8f05f15353b
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2020-10-26 11:22:34 -0300

    Last aula 17

--- Ex_83.py
+++ Ex_83.py
@@ -1,2 +1,21 @@
 #Crie um programa onde o usuário digite uma expressão qualquer que use parentesês. 
 #Seu aplicativo deverá analisar se a expressão passada está com os parentesês abertos e fechados na ordem correta
+
+expr = input("Digite a expressão: ")
+
+pilha = []
+
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')'
+        if len(pilha) > 0:
+            pilha.pop
+        else:
+            pilha.append(')')
+            break
+
+if len(pilha) == 0:
+    print("Sua Expressão esta válida")
+else:
+    print("Sua Expressão não esta válida")
