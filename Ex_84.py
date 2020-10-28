c7ba13b351de151984209386a91c823b326fd039
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2020-10-28 09:01:14 -0300

    Aula 17

---
+++ Ex_84.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista
+#No final mostre:
+#Quantas pessoas foram cadastradas
+#Uma listagem com as pessoas mais pesadas
+#Uma listagem com as pessoas mais leves
+
---
+++ Ex_85.py
@@ -0,0 +1,2 @@
+#Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em um lista única que mantenha separados os valores pares e impares
+#No final mostre os valores pares e impares em ordem crescente
