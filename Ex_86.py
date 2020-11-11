99ffae92869b5ecb609e2f39ec7cbafad150f6f9
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2020-11-10 22:00:04 -0300

    Last

--- Ex_86.py
+++ Ex_86.py
@@ -1,2 +1,12 @@
 #Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos do teclado
 #No final mostre a matriz na tela, com a formatação correta
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Digite um Valor para [{l}, {c}]: ')) 
+
+print('-=' *30)
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{matriz[l][c]}]', end='')
+    print()
